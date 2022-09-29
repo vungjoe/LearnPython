aeda23fb3fff6dcffd751ebdbc50c3bf9e392d5a
vungjoe <vungjoe@gmail.com>
2022-09-29 17:31:20 +0800

    图片转字符画

---
+++ 001-characterDraw.py
@@ -0,0 +1,13 @@
+#
+"""
+实战项目一
+将图片转换成由字符串组成的图画
+
+教程地址：http://www.coolpython.net/python_senior/miny_pro/char_picture.html
+"""
+from PIL import Image
+
+# 设定字符集
+
+char_set = '''$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. '''
+
